cd03b0b108d38d7859ac194aa288a6818bd24a57
Honan <jrp12@hampshire.edu>
2017-06-08 00:35:31 -0400

    big design changes...! Combined various locations

--- shared/design/locations.js
+++ shared/design/locations.js
@@ -16,29 +16,29 @@ LOCATIONS['townCenter'] = {
 	DESCRIPTION: 'A good place to chill and talk w ur team (look out for spies!)',
 	FLAVOR: 'some say papa bear was here a long time ago',
 	CAPACITY: 100,
-	EVENTS: [sameTeam.bind(null, 'guard'), enemyTeam.bind(null, 'invade'), enemyTeam.bind(null, 'sneak')],
+	EVENTS: [enemyTeam.bind(null, 'invade'), sameTeam.bind(null, 'craft'), sameTeam.bind(null, 'equip'), enemyTeam.bind(null, 'sneak'), enemyTeam.bind(null, 'steal')],
 	PRIVATE: true,
 	ACCESSIBLE: sameTeam.bind(null, true),
 	X_FROM_CENTER: 0,
 	Y_FROM_CENTER: 0,
 }
 
-LOCATIONS['supplyDepot'] = {
-	DESCRIPTION: 'All the shit is here, craft items, get equipment',
-	FLAVOR: 'papa bear smells something cooking!',
-	CAPACITY: 100,
-	EVENTS: [sameTeam.bind(null, 'guard'), enemyTeam.bind(null, 'invade'),  sameTeam.bind(null, 'craft'), sameTeam.bind(null, 'equip'), enemyTeam.bind(null, 'sneak'), , enemyTeam.bind(null, 'steal')],
-	PRIVATE: true,
-	ACCESSIBLE: sameTeam.bind(null, true),
-	X_FROM_CENTER: 1,
-	Y_FROM_CENTER: 1
-}
+// LOCATIONS['supplyDepot'] = {
+// 	DESCRIPTION: 'All the shit is here, craft items, get equipment',
+// 	FLAVOR: 'papa bear smells something cooking!',
+// 	CAPACITY: 100,
+// 	EVENTS: [enemyTeam.bind(null, 'invade'),  s],
+// 	PRIVATE: true,
+// 	ACCESSIBLE: sameTeam.bind(null, true),
+// 	X_FROM_CENTER: 1,
+// 	Y_FROM_CENTER: 1
+// }
 
 LOCATIONS['gate'] = {
 	DESCRIPTION: 'All the shit is here, craft items, get equipment',
 	FLAVOR: 'papa bear smells something cooking!',
 	CAPACITY: 100,
-	EVENTS: [sameTeam.bind(null, 'guard'), enemyTeam.bind(null, 'invade'), enemyTeam.bind(null, 'sneak')],
+	EVENTS: [enemyTeam.bind(null, 'invade'), enemyTeam.bind(null, 'sneak'), sameTeam.bind('trainWarfare')],
 	PRIVATE: false,
 	X_FROM_CENTER: 2,
 	Y_FROM_CENTER: 2
@@ -48,7 +48,7 @@ LOCATIONS['royalChambers'] = {
 	DESCRIPTION: 'Comfy! is that a nice velvet couch? (wehre the king stays)',
 	FLAVOR: 'The king of men resides here',
 	CAPACITY: 100,
-	EVENTS: [sameTeam.bind(null, 'guard')],
+	EVENTS: [],
 	PRIVATE: true,
 	ACCESSIBLE: sameTeam.bind(null, true),
 	X_FROM_CENTER: -1,
@@ -56,20 +56,20 @@ LOCATIONS['royalChambers'] = {
 }
 
 LOCATIONS['quarry'] = {
-	DESCRIPTION: 'This is where you mine ',
+	DESCRIPTION: 'This is where you mine',
 	FLAVOR: 'some say papa bear fell in here',
 	CAPACITY: 100,
-	EVENTS: ['guard', 'mine'],
+	EVENTS: ['mine'],
 	PRIVATE: false,
 	X_FROM_CENTER: 5,
 	Y_FROM_CENTER: 0
 }
 
 LOCATIONS['woods'] = {
-	DESCRIPTION: 'Theres is where you woodcut',
+	DESCRIPTION: 'A vast forest sits in front of you. You see a Wizards Tower poking out in the distance. These trees are a great place to hide....',
 	FLAVOR: 'some say papa bear LIVES here',
 	CAPACITY: 100,
-	EVENTS: ['guard', 'woodcut'],
+	EVENTS: ['woodcut', 'trainMagic', 'trainStealth'],
 	PRIVATE: false,
 	X_FROM_CENTER: 0,
 	Y_FROM_CENTER: 5
@@ -85,37 +85,37 @@ LOCATIONS['field'] = {
 	Y_FROM_CENTER: 5
 }
 
-LOCATIONS['barracks'] = {
-	DESCRIPTION: 'Theres is where you train warfare',
-	FLAVOR: 'some say papa bear wants to destroy this place',
-	CAPACITY: 100,
-	EVENTS: ['trainWarfare'],
-	PRIVATE: true,
-	ACCESSIBLE: sameTeam.bind(null, true),
-	X_FROM_CENTER: 0,
-	Y_FROM_CENTER: 2
-}
+// LOCATIONS['barracks'] = {
+// 	DESCRIPTION: 'Theres is where you train warfare',
+// 	FLAVOR: 'some say papa bear wants to destroy this place',
+// 	CAPACITY: 100,
+// 	EVENTS: ['trainWarfare'],
+// 	PRIVATE: true,
+// 	ACCESSIBLE: sameTeam.bind(null, true),
+// 	X_FROM_CENTER: 0,
+// 	Y_FROM_CENTER: 2
+// }
 
-LOCATIONS['sewers'] = {
-	DESCRIPTION: 'Theres is where you train stealth',
-	FLAVOR: 'some say papa bear has never seen this place',
-	CAPACITY: 100,
-	EVENTS: ['trainStealth'],
-	PRIVATE: true,
-	ACCESSIBLE: sameTeam.bind(null, true),
-	X_FROM_CENTER: 1,
-	Y_FROM_CENTER: 0
-}
+// LOCATIONS['sewers'] = {
+// 	DESCRIPTION: 'Theres is where you train stealth',
+// 	FLAVOR: 'some say papa bear has never seen this place',
+// 	CAPACITY: 100,
+// 	EVENTS: ['trainStealth'],
+// 	PRIVATE: true,
+// 	ACCESSIBLE: sameTeam.bind(null, true),
+// 	X_FROM_CENTER: 1,
+// 	Y_FROM_CENTER: 0
+// }
 
-LOCATIONS['tower'] = {
-	DESCRIPTION: 'Theres is where you train magic',
-	FLAVOR: 'some say you can see papa bear staring up here',
-	CAPACITY: 100,
-	EVENTS: ['trainMagic'],
-	ACCESSIBLE: sameTeam.bind(null, true),
-	PRIVATE: true,
-	X_FROM_CENTER: 1,
-	Y_FROM_CENTER: -1
-}
+// LOCATIONS['tower'] = {
+// 	DESCRIPTION: 'Theres is where you train magic',
+// 	FLAVOR: 'some say you can see papa bear staring up here',
+// 	CAPACITY: 100,
+// 	EVENTS: ['trainMagic'],
+// 	ACCESSIBLE: sameTeam.bind(null, true),
+// 	PRIVATE: true,
+// 	X_FROM_CENTER: 1,
+// 	Y_FROM_CENTER: -1
+// }
 
 module.exports = LOCATIONS
