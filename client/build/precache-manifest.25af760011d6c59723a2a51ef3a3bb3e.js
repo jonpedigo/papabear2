f95ec93420ce94b9d60465c0a45018fa19a99a5f
Jonathan Pedigo <jon.pedigo@feedly.com>
2019-08-26 00:52:45 -0700

    bundle file I think

---
+++ client/build/precache-manifest.25af760011d6c59723a2a51ef3a3bb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8ec8d2cf12d20691a204a63ecefd1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e2e2bc4eef0185d1034",
+    "url": "/static/css/main.995ff057.chunk.css"
+  },
+  {
+    "revision": "ba45cc3cb3cbd778448d",
+    "url": "/static/js/2.e41b1aff.chunk.js"
+  },
+  {
+    "revision": "7e2e2bc4eef0185d1034",
+    "url": "/static/js/main.2172a360.chunk.js"
+  },
+  {
+    "revision": "744dd6e0e6923e4f71f4",
+    "url": "/static/js/runtime~main.1b1cfb65.js"
+  }
+]);
